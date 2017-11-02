cdb6e10a23bac8ecca0b052e1fa90c7bce63d52b
Murakami <snowmanappli123@gmail.com>
2017-11-03 02:04:35 +0900

    進行中

---
+++ Assets/SceneManager_Battle.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneManager_Battle : MonoBehaviour {
+
+//------------------------------------------------------------------
+// シングルトン
+//------------------------------------------------------------------
+	public static SceneManager_Battle Instance;
+	private SceneManager_Battle(){
+		if( Instance == null ) Instance = this;
+	}
+
+//------------------------------------------------------------------
+// 初期化
+//------------------------------------------------------------------
+	public void Initialize(){
+
+	}
+
+	[SerializeField] XboxControllerManager xboxControlManager;
+	[SerializeField] XBoxInputType InputMode_charaMove; 
+	[SerializeField] XBoxInputType InputMode_windowControl; 
+	[SerializeField] XBoxInputType InputMode_pause; 	
+	private int SceneMode;
+	void Update(){
+		switch( SceneMode ){
+			case  1: 
+				xboxControlManager.InputController( InputMode_charaMove ); 
+			break;
+			case  2:
+				xboxControlManager.InputController( InputMode_windowControl ); 
+			break;
+			case  3:
+				xboxControlManager.InputController( InputMode_pause ); 
+			break;
+			default: 
+				xboxControlManager.InputController( InputMode_charaMove ); 
+			break;
+		}
+		
+	}
+
+//------------------------------------------------------------------
+// 処理
+//------------------------------------------------------------------
+	public void Open(){
+
+	}
+
+	public void Close(){
+		
+	}
+
+//------------------------------------------------------------------
+// ボタン
+//------------------------------------------------------------------
+	public void OnOKTouch(){
+
+	}
+
+}
---
+++ Assets/XboxControllerManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+
+
+//------------------------------------------------------------------
+// 入力
+//------------------------------------------------------------------
+public class XboxControllerManager : MonoBehaviour {
+	public void InputController( XBoxInputType inputType ){
+		if( Input.GetMouseButtonDown(0) ) inputType.ButtonA.Invoke();
+		if( Input.GetMouseButtonDown(1) ) inputType.ButtonB.Invoke();
+		if( Input.GetMouseButtonDown(2) ) inputType.ButtonX.Invoke();
+		if( Input.GetMouseButtonDown(3) ) inputType.ButtonY.Invoke();
+		if( Input.GetMouseButtonDown(4) ) inputType.ButtonLT.Invoke();
+		if( Input.GetMouseButtonDown(5) ) inputType.ButtonLB.Invoke();
+		if( Input.GetMouseButtonDown(6) ) inputType.ButtonRT.Invoke();
+		if( Input.GetMouseButtonDown(7) ) inputType.ButtonRB.Invoke();
+	}
+}
+
+
+//------------------------------------------------------------------
+// コントローラからのあらゆる入力を受け付けるコレクションクラス
+//------------------------------------------------------------------
+[System.Serializable]
+public class XBoxInputType : MonoBehaviour {
+	public UnityAction 
+	ButtonA,  ButtonB,  ButtonX,  ButtonY,
+	ButtonLT, ButtonLB, ButtonRT, ButtonRB,
+	ButtonSTART, ButtonBACK,
+	StickLeftPush, StickRightPush; 
+
+
+	[System.Serializable]
+	public class test : UnityEvent<float, float>  {
+		public UnityAction
+		StickLeft_UpDown, StickLeft_LeftRight,
+		StickRight_UpDown, StickRight_LeftRight,
+		DirectionPad_UpDown, DirectionPad_LeftRight; 
+	}
+}
